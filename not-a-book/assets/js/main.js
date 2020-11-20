021042e7fcd318cb74c47a88386afe60d062552e
angus-williams <angusmwilliams@gmail.com>
2020-11-20 22:39:36 +1100

    not a book upload

---
+++ not-a-book/assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    console.log('hello world');
+
+$('.zoom-in-1').click(function(){
+        $('.blur').addClass('active');
+      });
+
+$('.zoom-out-1').click(function(){
+        $('.blur').removeClass('active');
+      });
+
+
