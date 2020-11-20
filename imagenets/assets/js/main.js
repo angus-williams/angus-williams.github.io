11913ea4fced458e172b0fa0306e7147fdd17732
angus-williams <angusmwilliams@gmail.com>
2020-11-20 22:38:12 +1100

    imagenets upload

---
+++ imagenets/assets/js/main.js
@@ -0,0 +1,247 @@
+// function showImages(el) {
+//   var windowHeight = jQuery(window).height();
+//   $(el).each(function() {
+//     var thisPos = $(this).offset().top;
+
+//     var topOfWindow = $(window).scrollTop();
+//     if (topOfWindow + windowHeight - 200 > thisPos) {
+//       $(this).addClass("fadeIn");
+//     }else{
+//       $(this).removeClass("fadeIn");
+//     }
+//   });
+// }
+
+// $(window).scroll(function() {
+//   showImages('.image');
+// });
+
+$(document).ready(function(){
+  console.log('hello world');
+
+      $('.header').click(function(){
+        $('.intro').toggleClass('active');
+        $('.nav').toggleClass('active');
+      });
+
+      $('.one').click(function(){
+        $('.text').addClass('hide');
+        $('.text-one').removeClass('hide');
+        $('.text-one').addClass('active');
+      });
+
+      $('.two').click(function(){
+        $('.text').addClass('hide');
+        $('.text-two').removeClass('hide');
+        $('.text-two').addClass('active');
+      });
+
+      $('.two-2').click(function(){
+        $('.text').addClass('hide');
+        $('.text-two-2').removeClass('hide');
+        $('.text-two-2').addClass('active');
+      });
+
+      $('.three').click(function(){
+        $('.text').addClass('hide');
+        $('.text-three').removeClass('hide');
+        $('.text-three').addClass('active');
+      });
+
+      $('.four').click(function(){
+        $('.text').addClass('hide');
+        $('.text-four').removeClass('hide');
+        $('.text-four').addClass('active');
+      });
+
+      $('.five').click(function(){
+        $('.text').addClass('hide');
+        $('.text-five').removeClass('hide');
+        $('.text-five').addClass('active');
+      });
+
+      $('.six').click(function(){
+        $('.text').addClass('hide');
+        $('.text-six').removeClass('hide');
+        $('.text-six').addClass('active');
+      });
+
+      $('.seven').click(function(){
+        $('.text').addClass('hide');
+        $('.text-seven').removeClass('hide');
+        $('.text-seven').addClass('active');
+      });
+
+      $('.eight').click(function(){
+        $('.text').addClass('hide');
+        $('.text-eight').removeClass('hide');
+        $('.text-eight').addClass('active');
+      });
+
+      $('.nine').click(function(){
+        $('.text').addClass('hide');
+        $('.text-nine').removeClass('hide');
+        $('.text-nine').addClass('active');
+      });
+
+      $('.ten').click(function(){
+        $('.text').addClass('hide');
+        $('.text-ten').removeClass('hide');
+        $('.text-ten').addClass('active');
+      });
+
+      $('.eleven').click(function(){
+        $('.text').addClass('hide');
+        $('.text-eleven').removeClass('hide');
+        $('.text-eleven').addClass('active');
+      });
+
+      $('.twelve').click(function(){
+        $('.text').addClass('hide');
+        $('.text-twelve').removeClass('hide');
+        $('.text-twelve').addClass('active');
+      });
+
+      $('.thirteen').click(function(){
+        $('.text').addClass('hide');
+        $('.text-thirteen').removeClass('hide');
+        $('.text-thirteen').addClass('active');
+      });
+
+      $('.fourteen').click(function(){
+        $('.text').addClass('hide');
+        $('.text-fourteen').removeClass('hide');
+        $('.text-fourteen').addClass('active');
+      });
+
+      $('.fifteen').click(function(){
+        $('.text').addClass('hide');
+        $('.text-fifteen').removeClass('hide');
+        $('.text-fifteen').addClass('active');
+      });
+
+      $('.sixteen').click(function(){
+        $('.text').addClass('hide');
+        $('.text-sixteen').removeClass('hide');
+        $('.text-sixteen').addClass('active');
+      });
+
+      $('.seventeen').click(function(){
+        $('.text').addClass('hide');
+        $('.text-seventeen').removeClass('hide');
+        $('.text-seventeen').addClass('active');
+      });
+
+      $('.eighteen').click(function(){
+        $('.text').addClass('hide');
+        $('.text-eighteen').removeClass('hide');
+        $('.text-eighteen').addClass('active');
+      });
+
+      $('.nineteen').click(function(){
+        $('.text').addClass('hide');
+        $('.text-nineteen').removeClass('hide');
+        $('.text-nineteen').addClass('active');
+      });
+
+      $('.nineteen-2').click(function(){
+        $('.text').addClass('hide');
+        $('.text-nineteen-2').removeClass('hide');
+        $('.text-nineteen-2').addClass('active');
+      });
+
+      $('.twenty').click(function(){
+        $('.text').addClass('hide');
+        $('.text-twenty').removeClass('hide');
+        $('.text-twenty').addClass('active');
+      });
+
+      $('.twenty-2').click(function(){
+        $('.text').addClass('hide');
+        $('.text-twenty-2').removeClass('hide');
+        $('.text-twenty-2').addClass('active');
+      });
+
+      $('.twentyone').click(function(){
+        $('.text').addClass('hide');
+        $('.text-twentyone').removeClass('hide');
+        $('.text-twentyone').addClass('active');
+      });
+
+      $('.twentytwo').click(function(){
+        $('.text').addClass('hide');
+        $('.text-twentytwo').removeClass('hide');
+        $('.text-twentytwo').addClass('active');
+      });
+
+      $('.twentytwo-2').click(function(){
+        $('.text').addClass('hide');
+        $('.text-twentytwo-2').removeClass('hide');
+        $('.text-twentytwo-2').addClass('active');
+      });
+
+      $('.twentytwo-3').click(function(){
+        $('.text').addClass('hide');
+        $('.text-twentytwo-3').removeClass('hide');
+        $('.text-twentytwo-3').addClass('active');
+      });
+
+      $('.twentythree').click(function(){
+        $('.text').addClass('hide');
+        $('.text-twentythree').removeClass('hide');
+        $('.text-twentythree').addClass('active');
+      });
+
+      $('.twentyfour').click(function(){
+        $('.text').addClass('hide');
+        $('.text-twentyfour').removeClass('hide');
+        $('.text-twentyfour').addClass('active');
+      });
+
+      $('.twentyfive').click(function(){
+        $('.text').addClass('hide');
+        $('.text-twentyfive').removeClass('hide');
+        $('.text-twentyfive').addClass('active');
+      });
+
+
+
+
+
+
+      $('.btn-stone').click(function(){
+        $('.image').addClass('hide');
+        $('.image.stone').removeClass('hide');
+      });
+
+      $('.btn-marble').click(function(){
+        $('.image').addClass('hide');
+        $('.image.marble').removeClass('hide');
+      });
+
+      $('.btn-wood').click(function(){
+        $('.image').addClass('hide');
+        $('.image.wood').removeClass('hide');
+      });
+
+      $('.btn-metal').click(function(){
+        $('.image').addClass('hide');
+        $('.image.metal').removeClass('hide');
+      });
+
+      $('.btn-terracotta').click(function(){
+        $('.image').addClass('hide');
+        $('.image.terracotta').removeClass('hide');
+      });
+
+      $('.btn-reset').click(function(){
+        $('.image').removeClass('hide');
+      });
+
+      
+
+
+
+
+
+});
