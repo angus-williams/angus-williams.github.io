cd158ebb0289e8868af92d1506f302ceb19f070d
angus-williams <angusmwilliams@gmail.com>
2020-11-27 01:03:19 +1100

    online guide upload

---
+++ online-guide/assets/js/main.js
@@ -0,0 +1,65 @@
+// function showImages(el) {
+//   var windowHeight = jQuery(window).height();
+//   $(el).each(function() {
+//     var thisPos = $(this).offset().top;
+
+//     var topOfWindow = $(window).scrollTop();
+//     if (topOfWindow + windowHeight - 200 > thisPos) {
+//       $(this).addClass("fadeIn");
+//     }else{
+//       $(this).removeClass("fadeIn");
+//     }
+//   });
+// }
+
+// $(window).scroll(function() {
+//   showImages('.image');
+// });
+
+
+
+$(document).ready(function(){
+  console.log('hello world');
+
+      // $('.circle-1').click(function(){
+      //   $('.title-1').removeClass('active');
+      //   $('.title-2').removeClass('active');
+      //   $('.title-3').removeClass('active');
+      //   $('.title-4').removeClass('active');
+      // });
+
+      // $('.circle-2').click(function(){
+      //   $('.title-1').addClass('active');
+      //   $('.title-2').addClass('active');
+      //   $('.title-3').removeClass('active');
+      //   $('.title-4').removeClass('active');
+      // });
+
+      // $('.circle-3').click(function(){
+      //   $('.title-1').addClass('active');
+      //   $('.title-2').removeClass('active');
+      //   $('.title-3').addClass('active');
+      //   $('.title-4').removeClass('active');
+      // });
+
+      // $('.circle-4').click(function(){
+      //   $('.title-1').addClass('active');
+      //   $('.title-2').removeClass('active');
+      //   $('.title-3').removeClass('active');
+      //   $('.title-4').addClass('active');
+      // });
+
+
+
+
+      // $('.circle-3').click(function(){
+      //   $('.title-1').addClass('active');
+
+      //  setTimeout(
+      //       function() {
+      //           alert("");
+      //       },
+      //       1000);
+      //  });
+
+ 	});
